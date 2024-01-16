2f2efdec35380ec8ae6161d2821e689a777a4fbe
yuiseki <yuiseki@gmail.com>
2024-01-16 12:27:52 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   // TODO: You need to change this to your repo name
-  base: "/vite-deck-gl-map-template/",
+  base: "/vite-react-map-gl-deck-gl-template/",
 });
